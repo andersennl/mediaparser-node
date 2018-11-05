e34b22de9c292911b2fa67481a354607d2b1bfcc
Nikolaj Andersen <andersen.nl@icloud.com>
2018-11-05 17:16:13 +0100

    add config class

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export class Config {
+  static seriesPath(): string {
+    return "/Users/andersennl/iclouddrive/Coding/Node/mediaparser/media/testseries";
+  }
+
+  static moviesPath(): string {
+    return "/Users/andersennl/iclouddrive/Coding/Node/mediaparser/media/testmovies";
+  }
+}
