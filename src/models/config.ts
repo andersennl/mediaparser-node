e470f2ab49494a7f8d87441f2a8849bba46e1dc4
Nikolaj Andersen <andersen.nl@icloud.com>
2018-11-09 20:44:03 +0100

    correct local paths

--- src/models/config.ts
+++ src/models/config.ts
@@ -1,9 +1,9 @@
 export class Config {
   static seriesPath(): string {
-    return "/Users/andersennl/iclouddrive/Coding/Node/mediaparser/media/testseries";
+    return "/Users/andersennl/Coding/Node/mediaparser/media/testseries";
   }
 
   static moviesPath(): string {
-    return "/Users/andersennl/iclouddrive/Coding/Node/mediaparser/media/testmovies";
+    return "/Users/andersennl/Coding/Node/mediaparser/media/testmovies";
   }
 }
