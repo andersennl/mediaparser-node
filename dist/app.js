e36238fd3323672cf3b645ca28826a301180b3a1
Nikolaj Andersen <andersen.nl@icloud.com>
2018-12-03 22:19:40 +0100

    fix bug which prevents list from being updated

--- dist/app.js
+++ dist/app.js
@@ -9,11 +9,11 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../src/views"));
 app.use('/static', express.static('static'));
 app.disable("etag");
-let movieParser = new movieParser_1.MovieParser();
-let movies = movieParser.parse();
-let seriesParser = new seriesParser_1.SeriesParser();
-let series = seriesParser.parse();
 app.get("/", (req, res) => {
+    let movieParser = new movieParser_1.MovieParser();
+    let movies = movieParser.parse();
+    let seriesParser = new seriesParser_1.SeriesParser();
+    let series = seriesParser.parse();
     res.render('pages/index', { movies: movies, series: series });
 });
 app.listen(80, () => {
--- src/app.ts
+++ src/app.ts
@@ -11,13 +11,13 @@ app.set("views", path.join(__dirname, "../src/views"));
 app.use('/static', express.static('static'));
 app.disable("etag");
 
-let movieParser = new MovieParser();
-let movies = movieParser.parse();
-
-let seriesParser = new SeriesParser();
-let series = seriesParser.parse();
-
 app.get("/", (req: Request, res: Response) => {
+  let movieParser = new MovieParser();
+  let movies = movieParser.parse();
+
+  let seriesParser = new SeriesParser();
+  let series = seriesParser.parse();
+
   res.render('pages/index', { movies: movies, series: series });
 });
 
